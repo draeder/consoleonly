b652cb0d7bbecadc1c026bcf7d25e4b2f0d2f9db
draeder <draeder@gmail.com>
2022-05-19 00:50:01 -0500

    Reduced to 6 lines of code

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 export default function ConsoleOnly(){
-  const log = console.log
-  let c = 1
-  console.only = function(i, s) {
-    if(i && i == c){
-      log.apply(console, arguments)
-      c++
-    }
-  }
-  console.only.on = () => {
-    console.log = () => {}
-  }
-  console.only.off = () => {
-    console.log = log
-  }
+  const log = console.log; let c = 1
+  console.only = function(i, s) { if(i && i == c){ log.apply(console, arguments); c++ } }
+  console.only.on = () => { console.log = () => {} }
+  console.only.off = () => { console.log = log }
 } 
